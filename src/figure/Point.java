4dae980e9ab7757b039056c4e3f046b014c930bb
zhans24 <154365039+zhans24@users.noreply.github.com>
2023-12-23 12:39:36 +0600

    Update Point.java

--- src/figure/Point.java
+++ src/figure/Point.java
@@ -1,20 +1,25 @@
 package figure;
 import static java.lang.Math.*;
-// The Point class should be responsible of calculating the distance from the current point to the destination point.
+
 public class Point {
     private double x;
     private double y;
+    
     static int counter;
+    
     public Point(double x,double y){
         this.x=x;
         this.y=y;
+    
         counter++;
     }
-    //calculating distance
-    public double distance(Point dest){
 
+    
+    public double distance(Point dest){
         return sqrt(pow(dest.x-this.x,2)+pow(dest.y-this.y,2));
     }
+
+    
     public String toString(){
         return "added "+counter+" side ";
     }
